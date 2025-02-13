b23a3d5331845fda7dd7604900dbdc2671072032
DrDaNkk <39970384+nikko-guy@users.noreply.github.com>
2025-02-12 23:09:42 -0700

    implemented basic waypoint navigation

--- controllers/lab3_soln/lab3_soln.py
+++ controllers/lab3_soln/lab3_soln.py
@@ -46,19 +46,49 @@ gps.enable(SIM_TIMESTEP)
 compass = robot.getDevice("compass")
 compass.enable(SIM_TIMESTEP)
 
+def ramp_speed_func(curr_speed, target_speed, ramp_rate):
+    return curr_speed + ramp_rate * (target_speed - curr_speed)
+
+# use by doing ramp_speed(args)
+ramp_speed = np.vectorize(ramp_speed_func)
+
 # TODO: Find waypoints to navigate around the arena while avoiding obstacles
-waypoints = []
+waypoints = [(-0.308, -0.247, -np.pi/2), (-0.308, -0.445, 0), (0.335, -0.415, np.pi/2), (0.325, -0.245,np.pi), (0.08, -0.08,np.pi), (0.045, -0.01, np.pi/2),
+             (0.094, 0.056, 0), (0.325, 0.213, 0), (0.356, 0.262, np.pi/2), (0.321, 0.314, np.pi), (0.138, 0.42, 0), (-0.308, 0.427, 0), (-0.448, 0.3, 0), (-0.448, 0, 0)]
 # Index indicating which waypoint the robot is reaching next
 index = 0
 
 # Get ping pong ball marker that marks the next waypoint the robot is reaching
 marker = robot.getFromDef("marker").getField("translation")
 
-# Main Control Loop:
+def get_motor_speeds(dist_error,bearing_error,heading_error):
+    theta_epsilon = 1e-2
+    vR=0
+    vL=0
+    
+    #if dist error is >0.1
+        #if bearing error < theta_epsilon, drive straight
+    if(dist_error>0.05):
+        if(bearing_error>0 & bearing_error>theta_epsilon):
+            vL = ramp_speed(vL, -MAX_SPEED / 4, 0.1)
+            vR = ramp_speed(vR, MAX_SPEED / 4, 0.1)
+        else:
+            vL = ramp_speed(vL, MAX_SPEED / 4, 0.1)
+            vR = ramp_speed(vR, -MAX_SPEED / 4, 0.1)
+        #else, correct bearing error by turning
+    #if dist error <=0.1, minimize heading error
+    
+    return vR, vL
+
+controller_state = "turn_drive_turn_control"
+
 while robot.step(SIM_TIMESTEP) != -1:
     # Set the position of the marker
-    marker.setSFVec3f([waypoints[index][0], waypoints[index][1], 0.01])
-    
+    desired_x = waypoints[index][0]
+    desired_y = waypoints[index][1]
+    desired_theta = waypoints[index][2]
+    marker.setSFVec3f([desired_x, desired_y, 0.01])
+
     # Read ground sensor values
     for i, gs in enumerate(ground_sensors):
         gsr[i] = gs.getValue()
@@ -67,9 +97,48 @@ while robot.step(SIM_TIMESTEP) != -1:
     pose_x = gps.getValues()[0]
     pose_y = gps.getValues()[1]
     pose_theta = np.arctan2(compass.getValues()[0], compass.getValues()[1])
-    
+
     # TODO: controller
-    
-    print("Current pose: [%5f, %5f, %5f]" % (xr, yr, theta))
+
+    # calculate distance error
+    dist_error = np.sqrt((desired_x - pose_x) ** 2 + (desired_y - pose_y) ** 2)
+    # calculate bearing error
+    bearing_error = np.arctan2((desired_y - pose_y), (desired_x - pose_x)) - pose_theta
+    while(bearing_error<-np.pi):
+        bearing_error+=2*np.pi
+    while(bearing_error>np.pi):
+        bearing_error-=2*np.pi
+    # calculate heading error
+    heading_error = desired_theta - pose_theta
+
+    print("Current error: [%5f, %5f, %5f]" % (dist_error, bearing_error, heading_error))
+
+    if dist_error < 0.05:
+        index += 1
+        if index == len(waypoints):
+            index=0
+
+    # vR,vL = get_motor_speeds(dist_error,bearing_error,heading_error)
+
+    if controller_state == "turn_drive_turn_control":
+        if bearing_error > 0:
+            vL = ramp_speed(vL, -MAX_SPEED / 4, 0.1)
+            vR = ramp_speed(vR, MAX_SPEED / 4, 0.1)
+        else:
+            vL = ramp_speed(vL, MAX_SPEED / 4, 0.1)
+            vR = ramp_speed(vR, -MAX_SPEED / 4, 0.1)
+
+        if abs(bearing_error) < 0.05:
+            if dist_error > 0.05:
+                vL = ramp_speed(vL, MAX_SPEED, 0.1)
+                vR = ramp_speed(vR, MAX_SPEED, 0.1)
+            else:
+                vL = ramp_speed(vL, 0, 0.1)
+                vR = ramp_speed(vR, 0, 0.1)
+
+
+    print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
     leftMotor.setVelocity(vL)
     rightMotor.setVelocity(vR)
+
+    # turn_drive_turn_control state
